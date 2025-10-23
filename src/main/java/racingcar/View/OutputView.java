40eb6e3b09e97f6853567c1d9943bc19b6da6608
cyscys06 <cyscys06@gmail.com>
2025-10-23 16:57:42 +0900

    feat(Output): requestInput 메서드 추가

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -1,4 +1,7 @@
 package racingcar.View;
 
 public class OutputView {
+    public void requestInput() {
+        System.out.println(Message.REQUESTINPUT.getMessage());
+    }
 }
--- src/test/java/racingcar/View/OutputViewTest.java
+++ src/test/java/racingcar/View/OutputViewTest.java
@@ -30,7 +30,10 @@ public class OutputViewTest {
         @Test
         void 입력요구_출력_테스트() {
             outputView.requestInput(); // 나중에 만들 출력 메서드(println)
-            String requestMessage = bytearrayout.toString(); // 메서드의 반환(출력)이 바이트 배열에 저장된 것을 문자열 형태로 반환(toString)
+            String requestMessage = bytearrayout.toString().trim();
+            // 메서드의 반환(출력)이 바이트 배열에 저장된 것을 문자열 형태로 반환(toString)
+            // 그냥 toString만 쓰면 println이 개행 출력해서 개행도 같이 바이트 배열에 남음
+            // 그냥 print를 쓰기엔 사용자가 입력을 무조건 줄바뀜 후 해야하므로, trim으로 개행문자 제거하기
             assertThat(requestMessage).isEqualTo(Message.REQUESTINPUT.getMessage());
             // 레드코드 상태에선 바이트 배열에 저장된 값이 아무것도 없으므로 오류 뜸(파일끝에 도달했다는 메세지 출력)
         }
