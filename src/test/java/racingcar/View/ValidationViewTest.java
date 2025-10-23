cce08508336b8c1a37aeddd180d4ef1cc7a3f86c
cyscys06 <cyscys06@gmail.com>
2025-10-23 18:54:00 +0900

    refactor(ValidationTest): 자동차_이름에_빈_문자열_있는지_검증_테스트 리펙토링: 테스트케이스 수정, 메서드 이름 수정, 열거형 상수 이름 수정

--- src/test/java/racingcar/View/ValidationViewTest.java
+++ src/test/java/racingcar/View/ValidationViewTest.java
@@ -1,20 +1,25 @@
 package racingcar.View;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 public class ValidationViewTest {
-    private ValidationView validationView;
+    private ValidationView validationview;
+    @BeforeEach
+    void setUp() {
+        validationview = new ValidationView();
+    }
     @Nested
     @DisplayName("입력값 검증 테스트")
     class 입력값_검증_테스트 {
         @Test
         void 자동차_이름에_빈_문자열_있는지_검증_테스트() {
-            String input = "pobi; woni;jun";
-            assertThatIllegalArgumentException().isThrownBy(() -> validationview.CheckInputHaveWhiteSpace(input))
-                    .withMessageContaining(ErrorMessage.EMPTYSTRING.getMessage());
+            String input = "";
+            assertThatIllegalArgumentException().isThrownBy(() -> validationview.checkCarNameisEmpty(input))
+                    .withMessageContaining(ErrorMessage.EMPTYCARNAME.getMessage());
         }
     }
 }
