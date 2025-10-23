e8434c146c568c2a3be5b12262c525fd79b82a9c
cyscys06 <cyscys06@gmail.com>
2025-10-23 20:46:58 +0900

    feat(SplitInputTest): 자동차이름_배열_생성_테스트 추가

---
+++ src/test/java/racingcar/Controller/RaceControllerTest.java
@@ -0,0 +1,27 @@
+package racingcar.Controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class RaceControllerTest {
+    private CarController carController;
+
+    @BeforeEach
+    public void setUp() {
+        carController = new CarController();
+    }
+
+    @Nested
+    @DisplayName("컨트롤러 테스트")
+    class 컨트롤러_테스트 {
+        @Test
+        void 자동차이름_배열_생성_테스트() {
+            String input = "pobi;woni;jun";
+            String[] carnamearray = carController.makeCarNameArray(input);
+            assertThat(carnamearray).hasSize(3).containsExactly("pobi", "woni", "jun");
+        }
+    }
+}
