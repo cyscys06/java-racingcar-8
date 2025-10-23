b3a49ab5dbcd1f7845c4d78390bd548c9c10ff60
cyscys06 <cyscys06@gmail.com>
2025-10-23 18:58:08 +0900

    refactor(Validation): checkCarNameisEmpty 리펙토링: 누락된 getMessage() 메서드 추가

--- src/main/java/racingcar/View/ValidationView.java
+++ src/main/java/racingcar/View/ValidationView.java
@@ -3,7 +3,7 @@ package racingcar.View;
 public class ValidationView {
     public boolean checkCarNameisEmpty(String carname) {
         if (carname.isEmpty()) {
-            throw new IllegalArgumentException(ErrorMessage.EMPTYCARNAME);
+            throw new IllegalArgumentException(ErrorMessage.EMPTYCARNAME.getMessage());
         }
         return false;
     }
