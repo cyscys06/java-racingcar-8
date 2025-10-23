f1aa029b06457bff5a2fd1248cb1eb8fda60fce8
cyscys06 <cyscys06@gmail.com>
2025-10-23 22:29:31 +0900

    feat(OutputTest): requestAttemptsCount 메서드 추가

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public void requestInput() {
         System.out.println(Message.REQUESTINPUT.getMessage());
     }
+
+    public void requestAttemptsCount() {
+        System.out.println(Message.ATTEMPTS.getMessage());
+    }
 }
