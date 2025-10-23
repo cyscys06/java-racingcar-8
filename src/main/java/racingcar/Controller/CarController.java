c1056105689ebacc5866e091c9a6d0fb89b7bafa
cyscys06 <cyscys06@gmail.com>
2025-10-23 22:10:46 +0900

    feat(makeCarList): makeCarList 메서드 추가

--- src/main/java/racingcar/Controller/CarController.java
+++ src/main/java/racingcar/Controller/CarController.java
@@ -1,7 +1,20 @@
 package racingcar.Controller;
 
+import racingcar.Model.CarModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarController {
     public String[] makeCarNameArray(String input) {
         return input.split(",");
+    }
+
+    public List<CarModel> makeCarList(String[] carnamearray) {
+        List<CarModel> carList = new ArrayList<>(carnamearray.length);
+        for (int i = 0; i < carnamearray.length; i++) {
+            carList.add(new CarModel(carnamearray[i]));
+        }
+        return carList;
     }
 }
--- src/main/java/racingcar/Model/CarModel.java
+++ src/main/java/racingcar/Model/CarModel.java
@@ -5,7 +5,7 @@ public class CarModel {
     private int movementCount;
 
     // 이동 횟수는 전용으로 증가하는 메서드가 따로 존재해야함
-    CarModel(String carName) {
+    public CarModel(String carName) {
         this.carName = carName;
         movementCount = 0;
     }
