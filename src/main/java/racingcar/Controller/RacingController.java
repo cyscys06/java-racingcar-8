fa639f391262b1aa6fe81c856ed895996f66e68c
cyscys06 <cyscys06@gmail.com>
2025-10-24 21:05:46 +0900

    feat(getRandomNumber): getRandomNumber 메서드 추가

--- src/main/java/racingcar/Controller/RacingController.java
+++ src/main/java/racingcar/Controller/RacingController.java
@@ -1,5 +1,11 @@
 package racingcar.Controller;
 
-public class RacingController {
+import camp.nextstep.edu.missionutils.Randoms;
 
+public class RacingController {
+    public int getRandomNumber() {
+        final int LEASTRANDOMNUMBER = 0;
+        final int MAXRANDOMNUMBER = 9;
+        return Randoms.pickNumberInRange(LEASTRANDOMNUMBER, MAXRANDOMNUMBER);
+    }
 }
